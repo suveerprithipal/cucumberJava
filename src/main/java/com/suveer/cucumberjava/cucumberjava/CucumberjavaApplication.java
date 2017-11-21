25bc1878d927e6bad9d669796b39e19bb9e92338
Suveer Prithipal <suveerprithipal@Suveer.local>
2017-11-22 09:38:48 +1300

    New Vanilla Spring Project

---
+++ src/main/java/com/suveer/cucumberjava/cucumberjava/CucumberjavaApplication.java
@@ -0,0 +1,12 @@
+package com.suveer.cucumberjava.cucumberjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberjavaApplication.class, args);
+	}
+}
